942302cfa448673f441962ab7cec5cf5829970c4
xavierAshkar <xavierashkar@gmail.com>
2025-08-05 13:20:34 -0700

    fix(main.jsx): fix bug where an extra comma caused empty white space on page

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,6 @@ createRoot(document.getElementById('root')).render(
   <DndProvider backend={HTML5Backend}>
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   </DndProvider>
 )
